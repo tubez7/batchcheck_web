e420f2b4876c4f37c0483b8a89afb24fdc7f7a86
tubez7 <thescopeboys@hotmail.com>
2024-05-06 01:45:25 +0100

    complete refactor of stage 1 codebase build

---
+++ src/lib/components/EditIncrement.svelte
@@ -0,0 +1,51 @@
+<script>
+  // COMPONENTS
+  import SetIncrement from "$lib/components/SetIncrement.svelte";
+  import UpdateValuesButtons from "$lib/components/UpdateValuesButtons.svelte";
+
+  // PROPS
+  export let editFieldName;
+  export let validIncrement;
+  export let fieldClone;
+  export let field;
+  export let editSerial;
+  export let editIncrement;
+  export let editRecordsPerIncrement;
+  export let editPad;
+  export let editPrefix;
+  export let editSuffix;
+  export let editType;
+  export let changeMade;
+  export let hasSerial;
+  export let serialPadded;
+  export let padLead;
+  export let padTrail;
+
+  // VARIABLES
+  let incrementValue = fieldClone.incrementValue;
+  $: incrementValue = !editIncrement
+    ? fieldClone.incrementValue
+    : incrementValue;
+</script>
+
+<SetIncrement bind:incrementValue bind:validIncrement {hasSerial} />
+<UpdateValuesButtons
+  bind:fieldClone
+  bind:field
+  bind:editFieldName
+  bind:editSerial
+  bind:editIncrement
+  bind:editRecordsPerIncrement
+  bind:editPad
+  bind:editPrefix
+  bind:editSuffix
+  bind:editType
+  bind:changeMade
+  {hasSerial}
+  {serialPadded}
+  {padLead}
+  {padTrail}
+  value={incrementValue}
+  validCheck={validIncrement}
+  fieldToEditName="incrementValue"
+/>
